42faa208758869c3812dabf769d39305fac014b7
“MegaDJV” <vladimirsheglov2000@mail.ru>
2020-05-12 18:23:36 +0300

    qw

--- Wpf.BLPmodel.Pages.OtherPages/Views/ThreeView.xaml.cs
+++ Wpf.BLPmodel.Pages.OtherPages/Views/ThreeView.xaml.cs
@@ -42,6 +42,10 @@ namespace Wpf.BLPmodel.Pages.OtherPages.Views
         {
             sd.DataContext = new ReadNoteViewModel();
         }
+
+        private void sd_Navigated(object sender, NavigationEventArgs e) {
+
+        }
     }
 
 }
